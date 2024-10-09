c812b6e87bff2ba9a1c1f3c59278ea0f21a45a42
luci <lucille_blain@live.com>
2024-10-09 12:38:53 -0400

    added Next js and imported firbased config

---
+++ student-hustle/src/firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTVcSOnp-Cp6emQlRY0l6XZK1VBLXzWnc",
+  authDomain: "swen-746.firebaseapp.com",
+  projectId: "swen-746",
+  storageBucket: "swen-746.appspot.com",
+  messagingSenderId: "877097493850",
+  appId: "1:877097493850:web:b31b5d38831f9332217f63",
+  measurementId: "G-EHNTEY03T6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
