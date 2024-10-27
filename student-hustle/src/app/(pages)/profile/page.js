e467b36eab99244e655a9c71a81a7c7ad46335ce
Jose <jpalominolau@gmail.com>
2024-10-26 23:10:02 -0400

    Creation of empty pages

---
+++ student-hustle/src/app/(pages)/profile/page.js
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div>
+            <h1>Hello this is the profile</h1>
+        </div>
+    );
+}
---
+++ student-hustle/src/app/(pages)/register/page.js
@@ -0,0 +1,7 @@
+export default function Register() {
+    return (
+        <div>
+            <h1>Hello this is the register</h1>
+        </div>
+    );
+}
