b75266bc2d24f9e32d56b3e41bbdf75f2d2aa059
Vanessa <vn4261@rit.edu>
2024-11-15 16:35:36 -0500

    Adding new AC for Scheduler/Filters

---
+++ backend/src/api/request/SearchRequest.py
@@ -0,0 +1,8 @@
+from flask_restful import reqparse
+
+class SearchParser():
+    def __init__(self): 
+        pass # initalize 'title' to search for & 'price'
+
+    def get_parser(sef):
+        pass
