d6f72b5b88ff5bf8cb9d69c762323a2292b54180
Jose <jpalominolau@gmail.com>
2024-11-16 14:22:42 -0500

    Finished user login workflow

--- backend/src/api/request/UserRequest.py
+++ backend/src/api/request/UserRequest.py
@@ -2,7 +2,10 @@ from flask_restful import reqparse
 
 class UserParser:
     def __init__(self):
-        pass
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name', type=str, required=True, help='Please enter username')
+        self.parser.add_argument('password', type=str, required=True, help='Password cannot be empty')
+        self.parser.add_argument('email', type=str)
 
     def parse_args(self):
-        pass
+        return self.parser.parse_args()
--- backend/src/db/services/UserService.py
+++ backend/src/db/services/UserService.py
@@ -2,13 +2,71 @@ import os
 from ..swen610_db_utils import *
 
 def get_user(user_id):
-    pass
+    query = '''
+        SELECT *
+        FROM Users
+        WHERE id = %s;
+    '''
+
+    return exec_get_one(query, (user_id,))
+
+def get_user_by_username(username):
+    query = '''
+        SELECT *
+        FROM Users
+        WHERE name = %s;
+    '''
+
+    return exec_get_one(query, (username,))
 
 def create_user(data):
-    pass
+    existing_user = get_user_by_username(data['name'])
+
+    if existing_user:
+        return {"message": "User already exists!"}, 400
+
+    query = '''
+        INSERT INTO Users (name, password, email)
+        VALUES (%s, %s, %s);
+    '''
+
+    exec_commit(query, (data['name'], data['password'], data['email']))
+    return {"message": "User created successfully"}, 200
 
 def update_user(user_id, data):
-    pass
+    query = '''
+        UPDATE Users
+        SET name = %s, password = %s, email = %s
+        WHERE id = %s;
+    '''
+
+    rows_affected = exec_commit(query, (data['name'], data['password'], data['email'], user_id))
+
+    if rows_affected == 0:
+        return {"message": "User not found"}, 400
+    return {"message": "User updated successfully"}, 200
 
 def delete_user(user_id):
-    pass
+    query = '''
+        DELETE 
+        FROM Users
+        WHERE id = %s;
+    '''
+
+    rows_affected = exec_commit(query, (user_id,))
+
+    if rows_affected == 0:
+        return {"message": "User not found"}, 400
+    return {"message": "User deleted successfully"}, 200
+
+def verify_user(data):
+    query = '''
+        SELECT * FROM Users
+        WHERE name = %s AND password = %s;
+    '''
+
+    row_found = exec_get_one(query, (data['name'], data['password']))
+
+    if row_found:
+        return {"message": "Success"}, 200
+    return {"message": "Denied"}, 400
