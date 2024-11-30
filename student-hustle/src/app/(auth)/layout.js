b7638c1967b4ca238a244163c72c63d79cacb4c9
Jose <jpalominolau@gmail.com>
2024-11-29 20:41:11 -0500

    Example design with login, globals setup

--- student-hustle/src/app/(auth)/layout.js
+++ student-hustle/src/app/(auth)/layout.js
@@ -3,7 +3,6 @@ import "./auth.css";
 export default function AuthLayout({ children }) {
   return (
     <div>
-      <h2>THIS IS AUTH</h2>
       {children}
     </div>
   );
--- student-hustle/src/app/(auth)/login/page.js
+++ student-hustle/src/app/(auth)/login/page.js
@@ -1,6 +1,26 @@
+'use client';
+
+import { Card, CardBody, CardTitle, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
 export default function Login() {
-    return (
-      <div>User login</div>
-    );
-  }
-  
+  return (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <Card style={{ width: '300px' }}>
+        <CardBody>
+          <CardTitle tag="h4" className="text-center">Welcome to Student Hustle</CardTitle>
+          <Form>
+            <FormGroup>
+              <Label for="username">Username</Label>
+              <Input type="text" id="username" placeholder="Enter your username" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input type="password" id="password" placeholder="Enter your password" />
+            </FormGroup>
+            <Button color="primary" block>Login</Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
