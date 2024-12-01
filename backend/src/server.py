1220a7fee49edad67c1e4e1a07270f5c52c1b682
R Sai Sandeep <sandeepnaik9900@gmail.com>
2024-12-01 18:37:53 -0500

    Made few changes

--- backend/src/server.py
+++ backend/src/server.py
@@ -78,4 +78,4 @@ def on_join(data):
 
 if __name__ == '__main__':
     rebuild_tables()
-    socketio.run(app, debug=True)  # Use socketio.run instead of app.run
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)  # Use socketio.run instead of app.run
