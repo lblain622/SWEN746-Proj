e007d081408399c8e6a062210212143b87aea4cb
luci <lucille_blain@live.com>
2024-12-01 20:34:20 -0500

    Profile Page

--- student-hustle/src/app/(pages)/profile/page.js
+++ student-hustle/src/app/(pages)/profile/page.js
@@ -1,4 +1,8 @@
+import {useState} from "react";
 export default function Profile() {
+    const formData, setFormData=useState(
+
+    );
     return (
         <div>
             <h1>Hello this is the profile</h1>
