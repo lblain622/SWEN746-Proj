164f3c1838a2c40ac70089633921ca99c46c5908
luci <lucille_blain@live.com>
2024-12-02 18:45:45 -0500

    Pushing for tests

--- backend/tests/api/test_services.py
+++ backend/tests/api/test_services.py
@@ -64,6 +64,7 @@ class TestService(unittest.TestCase):
             'http://localhost:5000/services/user/1',
             get_header={'Content-Type': 'application/json'}
         )
+        api_result= api_result[0]
 
         api_result_filtered = [api_result[0], api_result[1], api_result[2], api_result[3], api_result[5]]
         self.assertEqual(api_result_filtered, expected_result, 'The service for a user is found')
